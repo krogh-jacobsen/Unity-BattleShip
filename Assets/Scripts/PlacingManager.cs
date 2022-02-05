346bac4e8ff01fd628b7b8b2ba6f8e25f430e08a
Thomas Krogh-Jacobsen <kroghjacobsen@gmail.com>
2022-02-05 13:34:30 +0100

    Placing ship ghost logic part I

--- Assets/Scripts/PlacingManager.cs
+++ Assets/Scripts/PlacingManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -58,5 +59,55 @@ public class PlacingManager : MonoBehaviour
         }
 
         // Place ghost
+    }
+
+    void ActivateShipGhost(int index)
+    {
+        if(index != -1)
+        {
+            if(shipList[index].shipGhost.activeInHierarchy)
+            {
+                return; 
+            }
+        }
+
+        // Deactivate all ghosts
+        for (int i = 0; i < shipList.Count; i++)
+        {
+            shipList[i].shipGhost.SetActive(false);
+        }
+
+        if(index == -1)
+        {
+            return;
+        }
+
+        //Activate needed ghost
+        shipList[index].shipGhost.SetActive(true);
+    }
+
+    void PlaceGhost()
+    {
+        if(isInPlacingMode)
+        {
+            canPlace = CheckForOtherShips();
+        }
+    }
+
+    private bool CheckForOtherShips()
+    {
+        foreach (Transform child  in shipList[currentShip].shipGhost.transform)
+        {
+            GhostBehavior ghost = child.GetComponent<GhostBehavior>();
+            if(!ghost.OverTile())
+            {
+                // Turn into read
+                child.GetComponent<MeshRenderer>().material.color = new Color32(255, 0, 0, 125);
+                return false;
+            }
+            // Turn back to white
+            child.GetComponent<MeshRenderer>().material.color = new Color32(0, 0, 0, 100);
+        }
+        return true; 
     }
 }
