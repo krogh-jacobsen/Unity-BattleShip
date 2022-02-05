20db3d5708d6ca7cb829d40ffd497156a4223a63
Thomas Krogh-Jacobsen <kroghjacobsen@gmail.com>
2022-02-05 11:57:28 +0100

    Updated ghostbehviour logic with raycast detection of what tile its placed on top of

--- Assets/Scripts/GhostBehavior.cs
+++ Assets/Scripts/GhostBehavior.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LayerMask layerToCheck;
+    RaycastHit raycastHit;      // Named hit in tutorial
+    TileInfo tileInfo;          // Named info in tutorial
+
+    Playfield playfield;
+
+    public void SetPlayfield(Playfield _playfield)
     {
-        
+        playfield = _playfield;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool OverTile()
     {
+        tileInfo = GetTileInfo();
+
+        if(tileInfo != null)
+        {
+
+        }
+
+        return false;
+    }
+
+    private TileInfo GetTileInfo()
+    {
+        // Shot downwards with ray to detect which tile the ghost is on top of
+        Ray ray = new Ray(transform.position, -transform.up);
         
+        if(Physics.Raycast(ray, out raycastHit, 1f, layerToCheck))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+            // Returning the tile which is just below the ghost
+            return raycastHit.collider.GetComponent<TileInfo>();
+        }
+
+        return null;
     }
 }
