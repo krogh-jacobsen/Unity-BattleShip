92e2401bc43ce044745f2584725dc85506105c18
Thomas Krogh-Jacobsen <kroghjacobsen@gmail.com>
2022-02-20 12:13:53 +0100

    Refactoring gamestate to be seperate class

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,13 @@
+﻿namespace NavyBattleGame
+{
+    public class GameState
+    {
+        public enum GameStates
+        {
+            P1_Place_Ships,
+            P2_Place_Ships,
+            Shooting,
+            Idle
+        }
+    }
+}
