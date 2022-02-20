4265a2f9940f4e6bad737c8dc924d8c0db17a3d1
Thomas Krogh-Jacobsen <kroghjacobsen@gmail.com>
2022-02-20 12:46:31 +0100

    Moving gameplay into games folder and creating systems folder

---
+++ Assets/CoreSystems/Audio/AudioType.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioType : MonoBehaviour
+{
+	#region Fields
+	//[Header("")]
+	[SerializeField]
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region MonoBehaviour
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
